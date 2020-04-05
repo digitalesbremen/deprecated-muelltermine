b53829bb3c492435d25a7c985f6159188784d589
Lars Michaelis <mail@larmic.de>
2020-04-05 21:01:05 +0200

    add some tests

--- loader/addressLoader_test.go
+++ loader/addressLoader_test.go
@@ -2,7 +2,6 @@ package loader
 
 import (
 	"testing"
-	"time"
 )
 
 func TestAddressLoader_LoadAddresses(t *testing.T) {
@@ -11,12 +10,12 @@ func TestAddressLoader_LoadAddresses(t *testing.T) {
 
 	verifyStreet(t, 0, `Aachener Straße`, addresses[0].Street)
 	verifyHouseNumber(t, 0, `2`, addresses[0].HouseNumber)
-	verifyCollectionDatesDate(t, 0, 0, "2020-03-09", addresses[0].CollectionDates[0].Date.Time)
-	verifyCollectionDatesDate(t, 0, 1, "2020-03-16", addresses[0].CollectionDates[1].Date.Time)
+	verifyCollectionDatesDate(t, 0, 0, "2020-03-09", []string{"BLUE", "YELLOW"}, addresses[0].CollectionDates[0])
+	verifyCollectionDatesDate(t, 0, 1, "2020-03-16", []string{"BLACK", "BROWN"}, addresses[0].CollectionDates[1])
 
 	verifyStreet(t, 1, `Waiblinger Weg`, addresses[1].Street)
 	verifyHouseNumber(t, 1, `33`, addresses[1].HouseNumber)
-	verifyCollectionDatesDate(t, 1, 0, "2020-03-11", addresses[1].CollectionDates[0].Date.Time)
+	verifyCollectionDatesDate(t, 1, 0, "2020-03-11", []string{"BLUE", "YELLOW"}, addresses[1].CollectionDates[0])
 }
 
 func verifyStreet(t *testing.T, addressIndex int, want string, got string) {
@@ -31,8 +30,25 @@ func verifyHouseNumber(t *testing.T, addressIndex int, want string, got string) 
 	}
 }
 
-func verifyCollectionDatesDate(t *testing.T, addressIndex int, collectionDatesIndex int, want string, got time.Time) {
-	if want != got.Format("2006-01-02") {
-		t.Errorf(`LoadAddresses()[%d].CollectionDates[%d].Date = %s; want %s`, addressIndex, collectionDatesIndex, got, want)
+func verifyCollectionDatesDate(t *testing.T, addressIndex int, collectionDatesIndex int, wantDate string, wantTypes []string, got GarbageCollectionDate) {
+	formattedGotDate := got.Date.Format("2006-01-02")
+	if wantDate != formattedGotDate {
+		t.Errorf(`LoadAddresses()[%d].CollectionDates[%d].Date = %s; want %s`, addressIndex, collectionDatesIndex, formattedGotDate, wantDate)
 	}
+
+	if !equal(wantTypes, got.Types) {
+		t.Errorf(`LoadAddresses()[%d].CollectionDates[%d].Types = %s; want %s`, addressIndex, collectionDatesIndex, got.Types, wantTypes)
+	}
+}
+
+func equal(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
