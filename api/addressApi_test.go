d0fadb4935a432842f707cd0eac3ec058aee923f
Lars Michaelis <mail@larmic.de>
2020-04-07 21:33:26 +0200

    add AddressesBuilder

--- api/addressApi_test.go
+++ api/addressApi_test.go
@@ -15,8 +15,20 @@ func TestAddressApi_LoadAddresses(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/api/address", nil)
 	res := httptest.NewRecorder()
 
-	var addresses []loader.Address
-	addresses = append(addresses, buildAddress("Langwedeler Str.", "1"))
+	addresses := newAddressBuilder().
+		withAddress("Langwedeler Straße", "1").
+		withAddress("Langwedeler Straße", "1a").
+		withAddress("Langwedeler Straße", "1b").
+		withAddress("Langwedeler Straße", "2").
+		withAddress("Langwedeler Straße", "3").
+		withAddress("Langwedeler Straße", "3a").
+		withAddress("Langwedeler Straße", "5").
+		withAddress("Langwedeler Straße", "5a").
+		withAddress("Langwedeler Straße", "5b").
+		withAddress("Zwoller Straße", "38").
+		withAddress("Zwoller Straße", "40").
+		withAddress("Zwoller Straße", "42").
+		build()
 
 	router := mux.NewRouter()
 	NewAddressesApi(addresses, router)
@@ -30,9 +42,32 @@ func TestAddressApi_LoadAddresses(t *testing.T) {
 		t.Errorf(`GET /api/address is no json: %s`, err.Error())
 	}
 
-	if dtos.Addresses[0] != "Langwedeler Str. 1" {
-		t.Errorf(`GET /api/address [0] = %s ; want %s`, dtos.Addresses[0], "Langwedeler Str. 1")
+	if dtos.Addresses[0] != "Langwedeler Straße 1" {
+		t.Errorf(`GET /api/address [0] = %s ; want %s`, dtos.Addresses[0], "Langwedeler Straße 1")
 	}
+}
+
+type AddressesBuilder struct {
+	addresses []loader.Address
+}
+
+func newAddressBuilder() AddressesBuilder {
+	return AddressesBuilder{addresses: []loader.Address{}}
+}
+
+func (b AddressesBuilder) withAddress(street string, houseNumber string) AddressesBuilder {
+	address := loader.Address{
+		Street:          street,
+		HouseNumber:     houseNumber,
+		CollectionDates: nil,
+	}
+
+	b.addresses = append(b.addresses, address)
+	return b
+}
+
+func (b AddressesBuilder) build() []loader.Address {
+	return b.addresses
 }
 
 func buildAddress(street string, houseNumber string) loader.Address {
